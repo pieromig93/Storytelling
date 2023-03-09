4bbdc2cadd9105b6d5d0c618a65e9dd4f344cba3
Piero <piero.migliorato@studenti.unicampania.it>
2023-03-09 17:51:53 +0100

    ADD: main.py file

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Rappresentare efficientemente una rete sottoforma di grafo. 
+
+La rete è composta da due strutture: 
+    - nodi
+    - archi orientati
+
+La rete, a sua volta, può rappresentare uno steriotipo di conversazione oppure una "storia". Quindi è presente una gerarchia di reti: la prima è una rete conversazionale
+l'altra 
+
+I nodi di tale rete rappresentano delle strutture d'interesse (possono essere dipinti, reperti archeologici),
+collegati ovviamente tra loro seguendo un ragionamento logico o fisico (per logico si intende culturale o filosofico, mentre per fisico si intende una vicinanza fisica 
+dei nodi, come ad esempio domus romane vicine )
+
+'''
