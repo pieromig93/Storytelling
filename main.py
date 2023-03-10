080f6c90d239af944937dd6b25449c01fd80e793
Piero <piero.migliorato@studenti.unicampania.it>
2023-03-10 14:37:12 +0100

    ADD: added Conversation Class

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ La rete è composta da due strutture:
     - nodi
     - archi orientati
 
-La rete, a sua volta, può rappresentare uno steriotipo di conversazione oppure una "storia". Quindi è presente una gerarchia di reti: la prima è una rete conversazionale
-l'altra 
+La rete, a sua volta, può rappresentare uno steriotipo di conversazione oppure una "storia". Quindi è presente una gerarchia di reti: 
+    - la prima è una rete conversazionale che rappresenta la tipologia di conversazione dovrà fare. Ad esempio parlando di Console romano 
+        (VITA)--->(STUDI)--->(OPERE)
+    
+    - la seconda è di tipo culturale/fisico. Ad esempio, se consideriamo l'area degli scavi di Pompei potremmo dire che sulla via X sono situate adiacentemente
+        domus (DOMUS 1)--->(DOMUS 2)--->(DOMUS 3)
+                       |--->(DOMUS 9)--->(DOMUS 10)
 
-I nodi di tale rete rappresentano delle strutture d'interesse (possono essere dipinti, reperti archeologici),
-collegati ovviamente tra loro seguendo un ragionamento logico o fisico (per logico si intende culturale o filosofico, mentre per fisico si intende una vicinanza fisica 
-dei nodi, come ad esempio domus romane vicine )
+Lo scopo di questo programma sarà quello di individuare quale percorso sul grafo risulta essere di maggior interesse per un utente che visita gli scavi ad esempio
+prendendo delle scelte in funzione di ciò che significativamente piace all'utente. Quindi considerandone un profilo con degli interessi pesati
 
 '''
+
+STATE_A, STATE_B, STATE_C = 0, 1, 2
+
+class Conversation:
+    actual_state = STATE_A
+
+    def get_state(self):
+        return self.actual_state
+
