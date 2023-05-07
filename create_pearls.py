a5ccbf208e021408a6c3e00a340775bbc858c48f
Piero <piero.migliorato@studenti.unicampania.it>
2023-05-07 10:45:34 +0200

    Added new way to create pearls

--- create_pearls.py
+++ create_pearls.py
@@ -1,6 +1,7 @@
 from owlready2 import *
+import json
 
-def clean_string(s):
+def remove_prefix(s):
     return s[9:]
 onto_path.append("/home/h93/Piero/Uni/Storytelling/")
 onto = get_ontology("ext_mara.owl").load()
@@ -12,13 +13,28 @@ ontology_class = list(default_world.sparql("""SELECT ?x WHERE{?x rdf:type owl:Cl
 canvas = list(default_world.sparql("""SELECT ?x WHERE{?x rdf:type <http://www.semanticweb.org/ontologies/2011/0/ProgettoAteneo2.owl#Canvas> } """))
 
 for i in range(len(canvas)):
-    query_subject = clean_string(str(canvas[i][0]))
+    query_subject = remove_prefix(str(canvas[i][0]))
     annotation_contained_into_canvas = list(default_world.
                                        sparql("""SELECT ?annotation WHERE{ 
                                                 <http://www.semanticweb.org/ontologies/2011/0/ProgettoAteneo2.owl#""" +query_subject+"""> 
                                                 <http://www.semanticweb.org/ontologies/2011/0/ProgettoAteneo2.owl#hasAnnotation>
                                                 ?annotation } """))
-    canvas[i].append(annotation_contained_into_canvas)
-    
+    clean_list = []
+    for an in annotation_contained_into_canvas:
+        clean_list.append(an[0])
+    canvas[i].append(clean_list)
+
+# in c[0] abbiamo tutti i canvas mentre in c[1] abbiamo la lista delle annotazioni
+annotation_detail_dict = {}
 for c in canvas:
-    print(c)
+    for i in range(len(c[1])):
+        query_subject = remove_prefix(str(c[1][i]))
+        detail_of_annotation = list(default_world.
+                                    sparql("""SELECT ?detail WHERE{ 
+                                                <http://www.semanticweb.org/ontologies/2011/0/ProgettoAteneo2.owl#""" +query_subject+"""> 
+                                                <http://www.semanticweb.org/ontologies/2011/0/ProgettoAteneo2.owl#hasDetail>
+                                                ?detail } """))
+        
+        annotation_detail_dict[query_subject] = remove_prefix(str(detail_of_annotation[0][0]))
+
+print(annotation_detail_dict)
