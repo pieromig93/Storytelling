7e0494f72c19a46d3d903b422e4bfd5b46fa27c7
Piero <piero.migliorato@studenti.unicampania.it>
2023-06-28 14:26:39 +0200

    general fixes and add new attribute

--- user.py
+++ user.py
@@ -1,16 +1,21 @@
 class User:
-    
-    preferences = []
-    canvas_in_pearl_counter = []
-    visited_canvas = []
-    visited_pearls = []
-    name = ""
-    awareness = []
-    satisfaction = []
-    def __init__(self, name, pref, route):
+    id = 0
+
+
+    def __init__(self, name, pref, route, color):
         self.name = name
         self.preferences = pref
-
+        self.id += 1
+        self.canvas_in_pearl_counter = []
+        self.visited_canvas = [] 
+        self.preferences = []
+        self.visited_canvas = []
+        self.visited_pearls = []
+        self.awareness = []
+        self.satisfaction = []
+        self.change_pearl_satisfaction_value = []
+        self.change_pearl_counter = 0
+        self.color = color
+        
         for i in range(len(route)):
             self.canvas_in_pearl_counter.append(int(0))
-            self.awareness.append(float(0))
